f41975b5e7ca12cf177c7ea71bd3ca0acda1a8fe
navalny-people <78288525+navalny-people@users.noreply.github.com>
2021-02-14 18:51:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let open_menu = document.querySelector('#open');
+let menu = document.querySelector('.menu')
+let close_menu = document.querySelector('#close')
+let close_ = document.querySelector('.close_')
+let section = document.querySelector('section')
+let info = document.querySelector('#info')
+
+let sectTop = () => section.offsetTop;
+let scrollMain = () => {
+    let page = window.pageYOffset;
+    page += 52;
+    return page;
+}
+
+open_menu.onclick = () => {
+    menu.style.display = 'block';
+    close_menu.style.display = 'block';
+    close_.style.display = 'block';
+    open_menu.style.display = 'none'
+}
+
+close_.onclick = () => {
+    menu.style.display = 'none';
+    close_menu.style.display = 'none';
+    close_.style.display = 'none';
+    open_menu.style.display = 'block'
+}
+
+close_menu.onclick = () => {
+    menu.style.display = 'none';
+    close_menu.style.display = 'none';
+    close_.style.display = 'none';
+    open_menu.style.display = 'block'
+}
+
+let colorBlock = () => {
+    if (scrollMain() >= sectTop()) {
+        open_menu.style.color = "#474747"
+    } else if (scrollMain() <= sectTop()) {
+        open_menu.style.color = "#ffffff"
+    }
+}
+
+window.addEventListener('scroll', colorBlock)
