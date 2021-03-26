81f2254830d67879bdd6d64cac391c0610234840
navalny-people <78288525+navalny-people@users.noreply.github.com>
2021-03-26 16:39:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let close_ = document.querySelector('.close_')
 let section = document.querySelector('section')
 let info = document.querySelector('#info')
 
+let widthDum = () => document.body.clientWidth;
 let sectTop = () => section.offsetTop;
 let scrollMain = () => {
     let page = window.pageYOffset;
@@ -41,4 +42,55 @@ let colorBlock = () => {
     }
 }
 
+let resizeDum = () => {
+    if (widthDum() >= 980) {
+        document.querySelector('span').style.display = 'none';
+        document.querySelector('.menu').style.display = 'flex'
+    } else {
+        document.querySelector('span').style.display = 'block';
+        document.querySelector('.menu').style.display = 'none';
+    }
+}
+
 window.addEventListener('scroll', colorBlock)
+window.addEventListener('resize', resizeDum)
+
+/*Слайдер*/
+
+/* Устанавливаем индекс слайда по умолчанию */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд*/
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливаем текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("item");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    /* Проходим по каждому слайду в цикле for */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
